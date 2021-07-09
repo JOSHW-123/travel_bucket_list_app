4b90189d804a1a3d7ba519bcb967291129dbf76e
Joshua Wilkinson <josh@Joshuas-MacBook-Pro.local>
2021-07-09 20:31:49 +0100

    city_repo.py select_all method added

--- repositories/city_repository.py
+++ repositories/city_repository.py
@@ -13,6 +13,29 @@ def save(city):
     city.id = id
     return city
 
+def select_all():
+    cities = []
+
+    sql = "SELECT * FROM cities"
+    results = run_sql(sql)
+
+    for row in results:
+        country = country_repository.select(row['country_id'])
+        city = City(row['name'], country, row['attractions'], row['temperature'], row['id'], row['visited'])
+        cities.append(city)
+    return cities
+
+# def select_all():
+#     tasks = []
+
+#     sql = "SELECT * FROM tasks"
+#     results = run_sql(sql)
+
+#     for row in results:
+#         user = user_repository.select(row['user_id'])
+#         task = Task(row['description'], user, row['duration'], row['completed'], row['id'] )
+#         tasks.append(task)
+#     return tasks
 
 # self.name = name
 # self.country = country
