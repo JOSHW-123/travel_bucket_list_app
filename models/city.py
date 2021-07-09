1ce1a590a7803ece2c59e58fee430575de9e4ede
Joshua Wilkinson <josh@Joshuas-MacBook-Pro.local>
2021-07-09 18:24:51 +0100

    Added parameters to classes

--- models/city.py
+++ models/city.py
@@ -1,8 +1,10 @@
 class City:
 
-    def __init__(self, name, attractions, visited = False):
+    def __init__(self, name, attractions, temperature, id = None, visited = False):
         self.name = name
         self.attractions = attractions
+        self.temperature = temperature
+        self.id = id
         self.visited = visited
 
     def mark_visited(self):
--- models/country.py
+++ models/country.py
@@ -1,6 +1,9 @@
 class Country:
 
-    def __init__(self, name, popular_destinations, visited = False):
+    def __init__(self, name, popular_destinations, population, language, currency, id = None, visited = False):
         self.name = name
         self.popular_destinations = popular_destinations
+        self.population = population
+        self.language = language
+        self.id = id
         self.visited = visited
