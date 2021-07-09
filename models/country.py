b38140228827fbf60c3491ed0e056a27ef1f79e5
Joshua Wilkinson <josh@Joshuas-MacBook-Pro.local>
2021-07-09 15:51:29 +0100

    country class created

---
+++ models/country.py
@@ -0,0 +1,6 @@
+class Country:
+
+    def __init__(self, name, popular_destinations, visited = False):
+        self.name = name
+        self.popular_destinations = popular_destinations
+        self.visited = visited
