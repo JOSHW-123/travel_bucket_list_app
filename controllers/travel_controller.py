a16b875e976a9a5fd35a670a1608186d4d08b1dc
Joshua Wilkinson <josh@Joshuas-MacBook-Pro.local>
2021-07-09 17:17:52 +0100

    .flaskenv populated with development code

--- controllers/travel_controller.py
+++ controllers/travel_controller.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 from werkzeug.utils import redirect
 from repositories import city_repository, country_repository
-from models. import Task
+from models.city import City
+from models.country import Country
 
-tasks_blueprint = Blueprint("tasks", __name__)
+travel_blueprint = Blueprint("travel", __name__)
