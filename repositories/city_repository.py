ef1f407a07c04378eb25233a3761de7691007358
Joshua Wilkinson <josh@Joshuas-MacBook-Pro.local>
2021-07-10 17:37:11 +0100

    update function added to city_repo

--- repositories/city_repository.py
+++ repositories/city_repository.py
@@ -44,6 +44,11 @@ def delete(id):
     sql = "DELETE FROM cities WHERE id = %s"
     values = [id]
     run_sql(sql, values)
+
+def update(city):
+    sql = "UPDATE cities SET (name, attractions, temperature, country_id, visited) = (%s, %s, %s, %s, %s) WHERE id = %s"
+    values = [city.name, city.country, city.attractions, city.temperature, city.country.id, city.visited, city.id]
+    run_sql(sql, values)
     
 # def select_all():
 #     tasks = []
