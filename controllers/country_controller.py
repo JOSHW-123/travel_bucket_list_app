081b1da2b45440f8d8b2d7db6e15fd746a1a9acd
Joshua Wilkinson <josh@Joshuas-MacBook-Pro.local>
2021-07-10 14:31:19 +0100

    countries/index.html route added to country controller

--- controllers/country_controller.py
+++ controllers/country_controller.py
@@ -7,3 +7,63 @@ from models.city import City
 
 
 countries_blueprint = Blueprint("countries", __name__)
+
+@countries_blueprint.route("/countries")
+def countries():
+    countries = country_repository.select_all()
+    return render_template("countries/index.html", all_countries=countries)
+
+# @tasks_blueprint.route("/tasks")
+# def tasks():
+#     tasks = task_repository.select_all()
+#     return render_template("tasks/index.html", all_tasks=tasks)
+
+# @tasks_blueprint.route("/tasks/new")
+# def new_task():
+#     users = user_repository.select_all()
+#     return render_template("tasks/new.html", all_users=users)
+
+# @tasks_blueprint.route("/tasks", methods=["POST"])
+# def create_task():
+#     description = request.form["description"]
+#     duration = request.form["duration"]
+#     completed = request.form["completed"]
+#     user_id = request.form["user_id"]
+#     user = user_repository.select(user_id)
+#     new_task = Task(description, user, duration, completed)
+#     task_repository.save(new_task)
+#     return redirect("/tasks")
+
+
+# # @tasks_blueprint.route("/tasks", methods=["POST"])
+# # def create_task():
+# #     print(request.form)
+# # This will show immutable object in terminal
+
+# @tasks_blueprint.route("/tasks/<id>/delete", methods=["POST"])
+# def delete_task(id):
+#     task_repository.delete(id)
+#     return redirect("/tasks")
+
+# @tasks_blueprint.route("/tasks/<id>", methods=["GET"])
+# def show_task(id):
+#     task = task_repository.select(id)
+#     return render_template("tasks/show.html", task = task)
+
+# @tasks_blueprint.route("/tasks/<id>/edit", methods=["GET"])
+# def edit_task(id):
+#     task = task_repository.select(id)
+#     users = user_repository.select_all()
+#     return render_template("tasks/edit.html", task = task,
+#     all_users = users)
+
+# @tasks_blueprint.route("/tasks/<id>", methods=['POST'])
+# def update_task(id):
+#     description = request.form['description']
+#     user_id     = request.form['user_id']
+#     duration    = request.form['duration']
+#     completed   = request.form['completed']
+#     user        = user_repository.select(user_id)
+#     task        = Task(description, user, duration, completed, id)
+#     task_repository.update(task)
+#     return redirect('/tasks')
